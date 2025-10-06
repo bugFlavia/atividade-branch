8932bbd17fbbacc6c244e7cbe7a4ef45257d0276
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2025-10-05 23:35:27 -0400

    feat: criar função de login simples

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Projeto atividade-branchs iniciado!");
+// login.js
+function login(usuario, senha) {
+    if (usuario === "admin" && senha === "1234") {
+        return "Login realizado com sucesso!";
+    }
+    return "Usuário ou senha inválidos.";
+}
+
+console.log(login("admin", "1234"));
+console.log(login("user", "senhaerrada"));
