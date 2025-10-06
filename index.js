11a19413408b5b2eb57b8155023375afc6b800de
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2025-10-05 23:36:06 -0400

    feat: adicionar validação mínima de caracteres no login

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // login.js
 function login(usuario, senha) {
+    if (usuario.length < 3 || senha.length < 3) {
+        return "Usuário e senha devem ter pelo menos 3 caracteres.";
+    }
+
     if (usuario === "admin" && senha === "1234") {
         return "Login realizado com sucesso!";
     }
     return "Usuário ou senha inválidos.";
 }
 
-console.log(login("admin", "1234"));
-console.log(login("user", "senhaerrada"));
+console.log(login("ad", "12"));  
+console.log(login("admin", "1234")); 
